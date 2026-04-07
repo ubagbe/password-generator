3d2aa054954beb5cfec023f386588ef8c6ca05b2
DivyanshChoubey <divyansh.choubey@agbeindia.com>
2026-04-07 16:53:33 +0530

    fix: updated main file

---
+++ main.py
@@ -0,0 +1,13 @@
+class Application:
+    def __init__(self):
+        pass
+
+    def show_menu(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    app = Application()
+    app.run()
