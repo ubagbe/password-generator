645bce1738c9419fd1fffd107bee34e301188f3c
DivyanshChoubey <divyansh.choubey@agbeindia.com>
2026-04-07 17:54:28 +0530

    feat: update main file

--- main.py
+++ main.py
@@ -3,10 +3,27 @@ class Application:
         pass
 
     def show_menu(self):
-        pass
+        print("-------------------------")
+        print("PASSWORD GENERATOR SYSTEM")
+        print("-------------------------")
+        print("1. Generate Password")
+        print("2. Check Password Strength")
+        print("3. Exit")
 
     def run(self):
-        pass
+        while True:
+            self.show_menu()
+            choice = input("please Enter You Choice : ")
+            if choice == "1":
+                print("Generate Password")
+            elif choice == "2":
+                print("Check Password Strength")    
+            elif choice == "3":
+                print("Exit")
+                break
+            else:
+                print("Invalid menu choice")    
+
 
 if __name__ == "__main__":
     app = Application()
