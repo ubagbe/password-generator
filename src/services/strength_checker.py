5a3d1975a25627f089c5a1ec5e3ec5a7e9b592a4
DivyanshChoubey <divyansh.choubey@agbeindia.com>
2026-04-09 15:02:34 +0530

    chore: snake_case name coversion & alignment

--- src/services/strength_checker.py
+++ src/services/strength_checker.py
@@ -4,13 +4,13 @@ import string
 class StrengthChecker:
 
     @classmethod
-    def checkpassword(cls, password):
+    def check_password(cls, password):
         length = len(password)
         digit = any(_ in string.digits for _ in password)
         lowercase = any(_ in string.ascii_lowercase for _ in password)
         uppercase = any(_ in string.ascii_uppercase for _ in password)
         special = any(_ in string.punctuation for _ in password)
-        
+
         if  (length > 12) and  special and digit and uppercase and lowercase:
             return("Strong Password")
         elif (10 <= length <= 12) and (digit or special) and uppercase and lowercase:
@@ -18,5 +18,4 @@ class StrengthChecker:
         elif (8 <= length <= 10) and uppercase and lowercase:
             return("Week Password")
         else:
-            return("Very Week Password")            
-       
+            return("Very Week Password")
